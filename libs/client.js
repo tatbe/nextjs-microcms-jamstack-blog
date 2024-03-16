62891918fe5f77f710ba54894b0f743a38fae382
tatbe <mugen.mountain@gmail.com>
2024-03-16 16:56:05 +0900

    done

--- libs/client.js
+++ libs/client.js
@@ -1,13 +1,6 @@
-// import { createClient } from "microcms-js-sdk";
-
-
-// export const client = createClient({
-//     serviceDomain: "darkun",
-//     apiKey: process.env.API_KEY
-// });
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "darkun",
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  apiKey: process.env.API_KEY,
 });
