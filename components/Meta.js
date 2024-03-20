1306d325cf4b468c91b050cb5f7842761721f03a
tatbe <mugen.mountain@gmail.com>
2024-03-20 12:52:47 +0900

    googleタグ追加

--- components/Meta.js
+++ components/Meta.js
@@ -6,8 +6,19 @@ function Meta({ title, description }) {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      {/* 他のメタデータもここに追加できます */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-SNFNEYEJX3"></script>
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-SNFNEYEJX3');
+                `,
+            }}
+        />
     </Head>
+    
   );
 }
 
